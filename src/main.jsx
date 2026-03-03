a9b119b169b89443c2996042f0683e5c1d71d69e
MatiasBebczuk <48242936@est.ort.edu.ar>
2026-03-03 17:03:37 -0300

    Place logo inside primary card stacks across screens

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import WebAlertHost from './ui/WebAlertHost.jsx'
-import GlobalBrand from './ui/GlobalBrand.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GlobalBrand />
     <App />
     <WebAlertHost />
   </StrictMode>,
